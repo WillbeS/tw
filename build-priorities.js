d7377aece9698ac061f2463a2150621e41d68a0c
plam <plam@localhost.dev>
2020-11-21 14:05:22 +0000

    Test

--- build-priorities.js
+++ build-priorities.js
@@ -1,6 +1,12 @@
+function getProductionData() {
+    const prodTable = window.$("#production_table");
+    
+    return prodTable;
+}
+
 try {
   if (game_data.screen == "overview_villages" && game_data.mode == "prod") {
-    console.log("Script logic goes here");
+    console.log(getProductionData());
   } else {
     UI.InfoMessage("Going to the combined overview...", 3000, "success");
     window.location = game_data.link_base_pure + "overview_villages&mode=prod";
